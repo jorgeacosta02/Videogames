edf26a07565a5625794a5e8446fb9618f020ec73
Jorge Acosta <jorgeacostadeleon@yahoo.com>
2023-05-25 01:13:35 -0300

    Modificación de succesForm

--- client/src/components/form/successForm.js
+++ client/src/components/form/successForm.js
@@ -1,60 +1,60 @@
-// import React from "react";
-// import styles from './successForm.module.css';
-// import { useDispatch, useSelector } from "react-redux";
-// import { changeSuccessFromBack } from "../../redux/actions";
-
-// const SuccessForm = ()=>{
-
-//     const successFromBack = useSelector(state => state.successFromBack);
-//     const dispatch = useDispatch();
-
-//     const cleanSuccessFromBackHandler = (value) => {
-//         return dispatch(changeSuccessFromBack(value))
-//     };
-
-//     return(
-//         <div className={styles.global}>
-//             <div className={styles.div}>
-//                 <p className={styles.message}>{successFromBack}</p>
-//                 <button className={styles.button} onClick={()=>cleanSuccessFromBackHandler('')}>Aceptar</button>
-//             </div>
-//         </div>
-//     )
-// };
-
-// export default SuccessForm;
-
-
-import React, { Component } from "react";
+import React from "react";
 import styles from './successForm.module.css';
-import { connect } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { changeSuccessFromBack } from "../../redux/actions";
 
-class SuccessForm extends Component {
-    cleanSuccessFromBackHandler = (value) => {
-        this.props.changeSuccessFromBack(value);
+const SuccessForm = ()=>{
+
+    const successFromBack = useSelector(state => state.successFromBack);
+    const dispatch = useDispatch();
+
+    const cleanSuccessFromBackHandler = (value) => {
+        return dispatch(changeSuccessFromBack(value))
     };
 
-    render() {
-        const { successFromBack } = this.props;
-
-        return (
-            <div className={styles.global}>
-                <div className={styles.div}>
-                    <p className={styles.message}>{successFromBack}</p>
-                    <button className={styles.button} onClick={() => this.cleanSuccessFromBackHandler('')}>Aceptar</button>
-                </div>
+    return(
+        <div className={styles.global}>
+            <div className={styles.div}>
+                <p className={styles.message}>{successFromBack}</p>
+                <button className={styles.button} onClick={()=>cleanSuccessFromBackHandler('')}>Aceptar</button>
             </div>
-        );
-    }
-}
-
-const mapStateToProps = state => ({
-    successFromBack: state.successFromBack
-});
-
-const mapDispatchToProps = {
-    changeSuccessFromBack
+        </div>
+    )
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(SuccessForm);
+export default SuccessForm;
+
+
+// import React, { Component } from "react";
+// import styles from './successForm.module.css';
+// import { connect } from "react-redux";
+// import { changeSuccessFromBack } from "../../redux/actions";
+
+// class SuccessForm extends Component {
+//     cleanSuccessFromBackHandler = (value) => {
+//         this.props.changeSuccessFromBack(value);
+//     };
+
+//     render() {
+//         const { successFromBack } = this.props;
+
+//         return (
+//             <div className={styles.global}>
+//                 <div className={styles.div}>
+//                     <p className={styles.message}>{successFromBack}</p>
+//                     <button className={styles.button} onClick={() => this.cleanSuccessFromBackHandler('')}>Aceptar</button>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// const mapStateToProps = state => ({
+//     successFromBack: state.successFromBack
+// });
+
+// const mapDispatchToProps = {
+//     changeSuccessFromBack
+// };
+
+// export default connect(mapStateToProps, mapDispatchToProps)(SuccessForm);
