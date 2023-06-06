6a4f78403ba98e91cfc4d727078d6f94f885e8d6
Jorge Acosta <jorgeacostadeleon@yahoo.com>
2023-06-06 12:24:08 -0300

    fix db genreRouter and videogameRouter

--- api/src/routes/genreRouter.js
+++ api/src/routes/genreRouter.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const getAllGenres = require('./genreControllers/genreGetAllController');
-const postGenre = require('./genreControllers/genrePostController');
+const getAllGenres = require('./genreControllers/genreGetAllController.js');
+const postGenres = require('./genreControllers/genrePostController.js');
 
 const genreRouter = express.Router();
 
 genreRouter.get('/', getAllGenres);
 
-genreRouter.post('/', postGenre);
+genreRouter.post('/', postGenres);
 
 
 module.exports = genreRouter;
