53a953ce395bb96920b67c00ab1b5c9ae46e5812
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-01-10 15:18:11 -0300

    redir2

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import Landing from './components/landingPage/landing.js';
-import Home from './components/main/home';
-import Form from './components/form/form';
-import One from './components/one/one';
-import NotFound from './components/notFound/notFound';
+// import './App.css';
+// import { Route, Switch } from 'react-router-dom';
+// import Landing from './components/landingPage/landing.js';
+// import Home from './components/main/home';
+// import Form from './components/form/form';
+// import One from './components/one/one';
+// import NotFound from './components/notFound/notFound';
 
 
-function App() {
+// function App() {
 
-  return (
-  <div className='App'>
-    <Switch>
-      <Route exact path='/'>
-        <Landing />
-      </Route>
-      <Route exact path='/videogames' render = { () => <div> <Home /> </div> } />
-      <Route path='/form'>
-        <Form />
-      </Route>
-      <Route exact path='/videogames/:id' render ={ () => <One />} />
-      <Route path='*' component = { NotFound }/>
-    </Switch>
-  </div>
-  );
-};
+//   return (
+//   <div className='App'>
+//     <Switch>
+//       <Route exact path='/'>
+//         <Landing />
+//       </Route>
+//       <Route exact path='/videogames' render = { () => <div> <Home /> </div> } />
+//       <Route path='/form'>
+//         <Form />
+//       </Route>
+//       <Route exact path='/videogames/:id' render ={ () => <One />} />
+//       <Route path='*' component = { NotFound }/>
+//     </Switch>
+//   </div>
+//   );
+// };
 
-export default App;
+// export default App;
