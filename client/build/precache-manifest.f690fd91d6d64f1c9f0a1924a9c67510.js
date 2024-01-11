591caaf682ed0b5c2129bbdab5452348f8258aaa
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-01-11 15:07:42 -0300

    build styles

---
+++ client/build/precache-manifest.f690fd91d6d64f1c9f0a1924a9c67510.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcaa8e21846127d971f4dec3636059a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0116c70c7c2711b163af",
+    "url": "/static/css/main.f7b54518.chunk.css"
+  },
+  {
+    "revision": "7818e917b359baa69490",
+    "url": "/static/js/2.62caa88c.chunk.js"
+  },
+  {
+    "revision": "ab8ccd89db8d0497c695",
+    "url": "/static/js/3.00b3093c.chunk.js"
+  },
+  {
+    "revision": "0116c70c7c2711b163af",
+    "url": "/static/js/main.75643401.chunk.js"
+  },
+  {
+    "revision": "cc27cffd0baafc1a17c6",
+    "url": "/static/js/runtime~main.6e7d3b15.js"
+  },
+  {
+    "revision": "1a523bb268d5948c61f4c40e65b7865f",
+    "url": "/static/media/img1.1a523bb2.jpg"
+  }
+]);
